fe2f929f309898d73511e7a4cec93adee8b2a8e6
mateusz <mateuszalmannai@googlemail.com>
2017-05-30 16:43:47 +0200

    get basics wired up

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    createItem(){
+      this.store.createRecord('post', {
+        title: this.get('title'),
+        body: this.get('body'),
+        author: this.get('author'),
+        date: this.get('date')
+      }).save();
+      this.set('title', '');
+      this.set('body', '');
+      this.set('author', '');
+      this.set('date','')
+    },
+    destroyItem(post){
+      post.destroyRecord();
+    }
+  }
 });
